baffbd1a117b7c1249aff1fec53d6c4ec9ce4990
TonyKerguen <tony.kerguen@etu.univ-orleans.fr>
2024-06-04 09:27:48 +0200

    Ajout d'un dossier image et des trois bouton du haut

--- src/fenetreAccueil.java
+++ src/fenetreAccueil.java
@@ -4,6 +4,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -13,10 +15,16 @@ import javafx.scene.text.FontWeight;
 public class fenetreAccueil extends BorderPane{
     
     private Button bjouer;
+    private Button boutonMaison;
+    private Button boutonParametres;
+    private Button boutonInfo;
 
-    public fenetreAccueil(Button bjouer){
+    public fenetreAccueil(Button bjouer, Button boutonMaison, Button boutonParametres, Button boutonInfo){
         super();
         this.bjouer = bjouer;
+        this.boutonMaison = boutonMaison;
+        this.boutonParametres = boutonParametres;
+        this.boutonInfo = boutonInfo;
         this.setTop(top());
         this.setCenter(center());
         // this.setBottom(bot());
@@ -27,39 +35,45 @@ public class fenetreAccueil extends BorderPane{
         Label textTop = new Label("Bienvenue sur le Puissance 4");
         textTop.setFont(Font.font("Arial", FontWeight.BOLD, 28));
         // Bouton sur la droite à rajouter
+        ImageView imageHome = new ImageView(new Image("file:img/home.png", 30, 30, true, true));
+        ImageView imageParam = new ImageView(new Image("file:img/parametres.png", 30, 30, true, true));
+        ImageView imageInfo = new ImageView(new Image("file:img/info.png", 30, 30, true, true));
+        this.boutonMaison = new Button("", imageHome);
+        this.boutonParametres = new Button("", imageParam);
+        this.boutonInfo = new Button("", imageInfo);
+        HBox trioBouton = new HBox(this.boutonMaison, this.boutonParametres, this.boutonInfo);
+        trioBouton.setSpacing(6);
         pane.setLeft(textTop);
+        pane.setRight(trioBouton);
         pane.setPadding(new Insets(15));
         return pane;
     }
 
     public VBox center(){
         VBox vbox = new VBox();
-        Label lj1 = new Label("Joueur 1");
-        Label lj2 = new Label("Joueur 2");
 
         Label labelChoisirNom = new Label("Choisissez un nom");
 
         HBox j1 = new HBox();
         j1.setSpacing(10);
         TextField tfj1 = new TextField();
-        j1.getChildren().addAll(lj1, tfj1);
+        j1.getChildren().addAll(new Label("Joueur 1"), tfj1);
 
         HBox j2 = new HBox();
         TextField tfj2 = new TextField();
-        j2.getChildren().addAll(lj2, tfj2);
+        j2.getChildren().addAll(new Label("Joueur 2"), tfj2);
         j2.setSpacing(10);
 
         Label labelChoisirCouleur = new Label("Choisissez une couleur");
 
         HBox cj1 = new HBox();
         ColorPicker colorPickerJ1 = new ColorPicker();
-        cj1.getChildren().addAll(lj1, colorPickerJ1);
+        cj1.getChildren().addAll(new Label("Joueur 2"), colorPickerJ1);
         cj1.setSpacing(10);
 
         HBox cj2 = new HBox();
-        
         ColorPicker colorPickerJ2 = new ColorPicker();
-        cj2.getChildren().addAll(lj2, colorPickerJ2);
+        cj2.getChildren().addAll(new Label("Joueur 2"), colorPickerJ2);
         cj2.setSpacing(10);
         
         this.bjouer = new Button("Lancer partie");
@@ -69,12 +83,4 @@ public class fenetreAccueil extends BorderPane{
         vbox.setSpacing(10);
         return vbox;
     }
-
-    // public VBox bot(){
-    //     VBox bot = new VBox();
-        
-    //     bot.getChildren().add(this.bjouer);
-    //     bot.setAlignment(Pos.CENTER_RIGHT);
-    //     return bot;
-    // }
 }
